8b088f0463a4b80bd9ce96dab9fa8305947e2c16
raphael <raphael.laurent@etu.univ-amu.fr>
2022-01-30 16:14:05 +0100

    ok le s

--- TP4/src/Stress1.java
+++ TP4/src/Stress1.java
@@ -8,28 +8,25 @@ import java.util.List;
 
 public class Stress1 {
     public static void main(String[] args) throws IOException {
-        int n = 2;
+        int n = Integer.parseInt(args[0]);
         int x = 0;
-        String str = "ok le s";
+        String str = "ok le S";
         byte[] bytes = str.getBytes();
         Long start = System.nanoTime();
         while (x != n){
             Socket s = new Socket("localhost", 1234);
             OutputStream out = s.getOutputStream();
             out.write(bytes);
-            s.close();
+            //s.close();
             x++;
         }
         Long[] tab = new Long[2];
         Long finish = System.nanoTime();
         Long time = finish - start;
-        tab[0] = new Long(n);
-        tab[1] = time;
-        Data.valeur_sans_close.add(tab);
-        for (int i =0;i<Data.valeur_sans_close.size();i++){
-            System.out.println(Data.valeur_sans_close.get(i)[0]);
-        }
-        System.out.println("taille de la liste : " + Data.valeur_sans_close.size());
-        System.out.println("le temps est : " + time );
+        //tab[0] = new Long(n);
+        //tab[1] = time;
+        //Data.valeur_sans_close.add(tab);
+        //System.out.println("taille de la liste : " + Data.valeur_sans_close.size());
+        System.out.println("le temps sans s.close est : " + time + " pour : " + n );
     }
 }
